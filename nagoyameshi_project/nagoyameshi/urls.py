2fe4f237056e585149e661a201d98150b4a2a729
IgnacioLuqueSanchez <161416788+IgnacioLuqueSanchez@users.noreply.github.com>
2024-06-19 14:59:30 +0900

    Add files via upload

---
+++ nagoyameshi_project/nagoyameshi/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+# views.pyをimportする。
+from . import views
+
+
+app_name    = "nagoyameshi"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("restaurant/", views.restaurant, name="restaurant" ),
+  
+
+]
